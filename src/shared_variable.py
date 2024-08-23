8feef7291a2b413c63f9eb1361332e735f7ba0e2
Tim-a-tree <jeonghyunheo619@gmail.com>
2024-08-23 09:41:23 +0900

    UPDATE: comments for shared variable

--- src/shared_variable.py
+++ src/shared_variable.py
@@ -1,1 +1,8 @@
+'''
+Holds the global(shared) variables of the project
+
+Variables:
+list_result = [] : list of the results those are needed for ExecutionResult and DetailedResult
+'''
+
 list_result = []
